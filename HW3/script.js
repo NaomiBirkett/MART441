a7ea8649853a009720451f7e7724258fb10600b6
NaomiBirkett <nb131357@umconnect.umt.edu>
2026-02-01 14:08:20 -0700

    HW3 Correction
    Push readme.md and script.js files to origin because they didn't go through the first time.

---
+++ HW3/script.js
@@ -0,0 +1,17 @@
+// Q&A portion
+var name = prompt("What is your name?");
+
+console.log("Hello, " + name);
+
+//My favorite websites
+console.log("My favorite websites are Instagram for browsing new updates from my favorite artists, Roll20 for playing D&D online, and Reddit for reading up on my favorite topics and discussing with other fans.");
+
+//My favorite games
+console.log("My favorite games are Baldur's Gate 3, Valheim, and Dungeons & Dragons.");
+
+//My favorites artists
+console.log("One of my favorite artists is Artemisia Gentileschi. She was a 17th century Italian painter who was one of the first women artists to succeed in a male-dominated field. Her experiences as a female artist in the 17th century and a sexual assault survivor lead her to create the, in my opinion, most moving portrayal of Judith Slaying Holofernes.");
+
+console.log("Another more recent favorite artist is Erika Williams aka HookieDuke. I especially love her detailed ink illustrations with minimal color use and they way she weaves her political beliefs and activism into her artwork.");
+
+console.log("A local famous artist of mine is Stella Nall. I love her creative and whimsical way of depicting animals, her incorporation of beadwork into her paintings, and the way she weaves her identity as a first descendant of Apsáalooke tribe into her art.");
